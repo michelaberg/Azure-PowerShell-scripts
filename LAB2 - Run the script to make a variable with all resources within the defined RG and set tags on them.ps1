f45c15f0d6b33db1be7d1dbe0fca100261cde8ee
michelaberg <michel.aaberg@live.se>
2024-09-12 23:13:03 +0200

    Add files via upload

---
+++ LAB2 - Run the script to make a variable with all resources within the defined RG and set tags on them.ps1
@@ -0,0 +1,25 @@
+﻿# Define the resource group name
+$resourceGroupName = "MyResourceGroup" 
+
+# Retrieve and store all resources in the specified resource group into a variable
+$resourcesList = Get-AzResource -ResourceGroupName $resourceGroupName
+
+# Loop through each resource and apply a tag
+foreach ($resource in $resourcesList) {
+    # Get existing tags (if any)
+    $currentTags = $resource.Tags
+    
+    # Add or update the 'Owner' tag
+    $newTags = @{"Owner" = "Michel"}
+
+    # If there are existing tags, merge them with the new tag
+    if ($currentTags) {
+        $newTags = $currentTags + $newTags
+    }
+
+    # Set the new tags on the resource
+    Set-AzResource -ResourceId $resource.ResourceId -Tag $newTags -Force
+
+    # Output the result
+    Write-Output ("Tagged resource: " + $resource.Name + " with Owner=Michel")
+}
